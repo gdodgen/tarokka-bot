5c6d9cbeb5fad06d4d7d380e6a1b98d185ba3fed
gdodgen <gracecdodgen@gmail.com>
2021-09-09 05:50:47 -0700

    bot responds to messages

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
 
 const {Client, Intents} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS)
+const client = new Client({ intents: myIntents });
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} is running!`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === '!test') {
+        message.channel.send('hello');
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
